2a070ee03f71ece26a488e41f8ef72edafc13e79
Gaurav kumar <gauravraaz2005@gmail.com>
2024-02-03 00:21:48 +0530

    backend upto profile

---
+++ server/index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv"
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from 'path';
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import { register } from "./controllers.auth.js";
+
+import { verifyToken } from "./middleware/auth.js";
+
+// CONFIGRATIOAN
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgon("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb",extended:true}));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname,'public/assets')));
+
+// FILE STORAGE
+/**
+ these information is coming from package instruction 
+ from the github repo of multer
+ By this we can save the file if anyone upload file on app ,
+ then it is save in th "public/assets" folderr
+ */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null,"public/assets");
+    },
+    filename: function ( req,file,cb){
+        cb(null,file.originalname);
+    }
+});
+const upload = multer({ storage}); //this help us to save
+
+//ROUTES WITH FILES
+app.post("/auth/register",upload.single("picture"), register);
+
+//ROUTES
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+
+//MONGOOSE SETUP
+const PORT = process.env.PORT || 6001;
+mongoose
+ .connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+ })
+ .then(() =>{
+    app.listen(PORT,()=> console.log('server Port: ${PORT}'));
+ })
+ .catch((error) => console.log('${error} did not connect'));
