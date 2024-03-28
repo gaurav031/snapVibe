e8a29ef14cacb8841e9b31d001a0c111b3008b75
Gaurav kumar <gauravraaz2005@gmail.com>
2024-03-28 14:33:41 +0530

    header done

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,4 +1,9 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
+
 const HomePage = () => {
-    return <div>homeppage</div>
+    return <Box>
+        <Navbar />
+    </Box>>
 };
 export default HomePage;
