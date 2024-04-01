bd2de64ca7cf8b83e9c873b49bd8cceac63c9a25
Gaurav kumar <gauravraaz2005@gmail.com>
2024-04-02 02:09:12 +0530

    minor change

--- server/models/post.js
+++ server/models/post.js
@@ -1,31 +1,35 @@
 import mongoose from "mongoose";
 
-const postSchema = mongoose.Schema({
+const postSchema = mongoose.Schema(
+  {
     userId: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     firstName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     lastName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     location: String,
     description: String,
     picturePath: String,
     userPicturePath: String,
     likes: {
-        type: Map,
-        of: Boolean,
+      type: Map,
+      of: Boolean,
     },
     comments: {
-        type: Array,
-        default: []
-    }
-}, { timestamps: true });
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
 
-const PostModel = mongoose.model("Post", postSchema); // Create model from schema
-export default PostModel; // Export the model
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
