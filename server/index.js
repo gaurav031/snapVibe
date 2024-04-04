8ae7b74f2d3188b583970c3cbe74f3918a5d5300
Gaurav kumar <gauravraaz2005@gmail.com>
2024-04-05 01:28:52 +0530

    update mongoose package

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
-import  Post from "./models/Post.js";
+import Post from "./models/post.js";
 import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
